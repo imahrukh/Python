a3294b8b5dcab304a3be215a9217713103058cf3
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-30 22:57:47 +0500

    creating variables  in pyton

---
+++ Print/variables.py
@@ -0,0 +1,20 @@
+#creating avriables
+print("creating variables: ")
+x = 5
+y = "John"
+print("x: "+x)
+print("y: "+y)
+#changing type after declaring
+print("changing types: ")
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print("string x: "+x)
+#type casting
+print("type casting: ")
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print("x(string): ":+ x)
+print("y(integer): "+y)
+print("z(float): "+z)
+
