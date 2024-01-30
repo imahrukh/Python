59de1a289352314d1663e6d070062428e9e0a943
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-30 23:48:44 +0500

    printing output of variables

---
+++ Print/output.py
@@ -0,0 +1,18 @@
+print("Printing single variable: ")
+x = "Python is awesome"
+print(x)
+print("Printing multiple variables: ")
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+print("using + operator: ")
+print(x + y + z)
+print(" using + as mathematical operator: ")
+x = 5
+y = 10
+print(x + y)
+print("comma separated output: ")
+x = 5
+y = "John"
+print(x, y)
