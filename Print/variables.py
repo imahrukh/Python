9b3fcc4481d6e3b8d1a4c43d22be618ed15600d4
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-31 00:53:06 +0500

    created and intialized variables

--- Print/variables.py
+++ Print/variables.py
@@ -2,8 +2,10 @@
 print("creating variables: ")
 x = 5
 y = "John"
-print("x: "+x)
-print("y: "+y)
+print("x: ")
+print (x)
+print("y: ")
+print(y)
 #changing type after declaring
 print("changing types: ")
 x = 4       # x is of type int
@@ -14,24 +16,8 @@ print("type casting: ")
 x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
 z = float(3)  # z will be 3.0
-print("x(string): ":+ x)
-print("y(integer): "+y)
-print("z(float): "+z)
-#getting types of variables
-print("Getting the type: " )
-x = 5
-y = "John"
-print(type(x))
-print(type(y))
-#using single and double quotes for string types
-print("single and double quotes: ")
-x = "John"
-print("x with double quotes: "+ x)
-x = 'John'
-print("x with single quotes: " +y )
-#case sensitive
-print("case sensitive: ")
-a = 4
-print("a  small case: "+a)
-A = "Sally"
-print("A  capital case: "+A)
+print("x(string): ")
+print(x)
+print("y(integer): ")
+print(y)
+print("z(float): ")
