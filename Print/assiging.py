349f7f046c359c190436d4b633efb4624dc002ec
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-30 23:31:40 +0500

    assigning values to multiple variables

---
+++ Print/assiging.py
@@ -0,0 +1,16 @@
+print("Assigning values to multiple variables in a line :")
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+print("Assigning same value to multiple variables")
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+print("unpacking values ina collection:")
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
