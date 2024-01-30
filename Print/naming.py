a93d0206bd1a2fdb2d5bd236b98b282f8db36b0d
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-30 23:26:54 +0500

    naming conventions

---
+++ Print/naming.py
@@ -0,0 +1,20 @@
+print("Naming: ")
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print("Naming convention: ")
+#camel case
+print("Camel case :")
+myVariableName = "John"
+print("myVariableName: "+myVariableName)
+#pascal case
+print("Pascal case :")
+MyVariableName = "John"
+print("MyVariableName: "+MyVariableName)
+#snake case
+print("Snake case :")
+my_variable_name = "John"
+print("my_variable_name: "+my_variable_name)
