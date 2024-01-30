c0212a4bf10b2d2dfcccd264e974b233a22730f6
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-31 01:03:46 +0500

    data types for numbers

---
+++ dataTypes/numbers.py
@@ -0,0 +1,74 @@
+print("Assigning numerical values to variables")
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+
+print("Integers: ")
+x = 1
+y = 35656222554887711
+z = -3255522
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+print("float numbers: ")
+x = 1.10
+y = 1.0
+z = -35.59
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+
+print("complex numbers: ")
+x = 3+5j
+y = 5j
+z = -5j
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+
+print("Type conversions: ")
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
